744358b779dfdd94d54d5979d8e1b43696f6b258
Derek Surguy <Surguy.Derek@gmail.com>
2015-07-10 13:02:02 -0500

    Updating config for 1.3.0.4

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 	//url to the latest release of the dedicated server software
-	dedicatedServerDownload: 'http://terraria.org/system/dedicated_servers/archives/000/000/005/original/terraria-server-1303.zip',
+	dedicatedServerDownload: 'http://terraria.org/system/dedicated_servers/archives/000/000/005/original/terraria-server-1304.zip',
 	//directory that the server .zip will be downloaded to
 	downloadDirectory: './Server',
 	//directory to run the server in (.zip files will be extracted here)
